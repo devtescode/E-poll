39088384f5eb986b6fb7ca33331e0bfb5ce4cf2b
TESCODE <teslimagboola09@gmail.com>
2024-07-08 15:15:27 +0100

    E-voting frontend

--- src/app/page.js
+++ src/app/page.js
@@ -4,91 +4,234 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <nav className="navbar navbar-expand-lg bg-body-white">
+        <div className="container-fluid">
+          <div className="Epoll_img_navbar">
+          </div>
+          {/* <a className="navbar-brand" href="#">E-poll</a> */}
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Register</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Login</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">Dashboard</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+
+      <div className="lp_textdiv gap-5">
+        <div className="lp_first_div">
+          <div className="text-white">
+            <h1>Enhance your voting experience with E-poll</h1>
+            <p>It's secure, transparent, and easy to use. Make your vote count effortlessly!</p>
+            <div>
+              <button className="btn btn-success" style={{ backgroundColor: "#228B22" }}>Vote Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="lp_second_div">
+
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <div className="lp_Epoll_movedown mx-5">
+          <div className="lp_Epoll_intro">
+            <h1 style={{ fontSize: "50px" }} className="">Transform your decision-making process with E-poll</h1>
+          </div>
+          <div className="lp_Epoll_ptag">
+            <p>
+              modern, secure, and effective. Upgrade from manually counting ballots to an online election system, ensuring the integrity of your vote. Embrace the future of voting with confidence!
+            </p>
+          </div>
+        </div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <div className="lp_hover_page">
+        <div className="card-container">
+          <div className="card2">
+            <div className="front-content">
+              <div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                <div className="lp_imgone">
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                </div>
+                <div className="text-center">
+                  <p>Secure</p>
+                </div>
+              </div>
+            </div>
+            <div className="content">
+              {/* <p className="heading">Card Hover</p> */}
+              <p>
+                E-poll ensures your vote is confidential, tamper-proof, and accurately counted. With top-notch encryption and authentication, you can trust your vote is secure. Make your voice heard with E-poll.
+              </p>
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="card-container">
+          <div className="card2">
+            <div className="front-content">
+              <div>
+
+                <div className="lp_imgtwo">
+
+                </div>
+                <div className="text-center">
+                  <p>Import Ballot & Voters</p>
+                </div>
+              </div>
+            </div>
+            <div className="content">
+              {/* <p className="heading">Card Hover</p> */}
+              <p>
+                Save time with E-poll by importing your voters and ballots from an Excel spreadsheet or CSV file. Our streamlined process makes setting up your election quick and easy, ensuring a smooth and efficient voting experience.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="card-container">
+          <div className="card2">
+            <div className="front-content">
+              <div>
+
+                <div className="lp_imgthree">
+
+                </div>
+                <div className="text-center">
+                  <p>Results Tabulation</p>
+                </div>
+              </div>
+            </div>
+            <div className="content">
+              {/* <p className="heading">Card Hover</p> */}
+              <p>
+                E-poll automatically calculates election results and presents them with beautiful charts, making it easy to visualize and understand the outcomes.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="lp_bg_cover_color">
+        <div className="custom-shape-divider-bottom-1720443896">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+          </svg>
+        </div>
+        <div className="lp_inner_bg_text">
+          <div className="lp_inner_width_text p-4">
+            <h1 className="fw-bold" style={{ fontSize: "60px" }}>Election security you can rely on</h1>
+            <p>Our E-poll security measures protect you, your data, and your organization against risk.</p>
+            <div className="d-flex">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+              </div>
+              <div className="mx-1">
+              <p>
+              Our e-poll security measures protect your data and organization. Ensure vote integrity with robust auditing tools and eliminate double voting and fraud through closed voting events for trustworthy decision-making.</p>
+              </div>
+            </div>
+            <div className="d-flex">
+              <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
+                </svg>
+              </div>
+              <div className="mx-1">
+              <p>
+              Our e-poll security measures protect you, your data, and your organization against risk. Ensure vote integrity with our robust auditing tools that demonstrate an accountable voting process.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lp_inner_bg_color">
+
+        </div>
+      </div>
+
+      <div className="About_Epoll_div">
+        <div className="First_Epoll_about_div">
+        </div>
+        <div className="Second_Epoll_about_div">
+          <div className="mx-3">
+            <h2>Why Choose Us</h2>
+          </div>
+          <div className="mx-3">
+            <p>
+              E-poll offers a secure, user-friendly, and efficient voting experience. Our platform features advanced encryption for tamper-proof voting and an intuitive interface for easy voter and ballot import from Excel or CSV files. We provide automated, transparent results with clear charts and ensure accessibility from any device, anywhere in Nigeria. Supported by a dedicated team, E-poll guarantees a reliable and cost-effective solution. Choose E-poll for a modern, trustworthy, and seamless voting process.
+            </p>
+          </div>
+          <div className="mx-3">
+            <button className="btn btn-success" style={{ backgroundColor: "#228B22" }}>Read-more</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="lp_footer_div">
+        <div className="lp_footer_div_one">
+          <div className="">
+            <div>
+
+              <div className="Epoll_img_navbar">
+              </div>
+              <div className="mx-4 mt-3">
+                <p>
+                  E-poll is dedicated to empowering voters and driving positive change through secure, accessible, and efficient digital elections.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="lp_footer_div_two">
+          <div>
+            <h2>Contact Us</h2>
+          </div>
+          <div>
+            <p>
+              Email: epoll@org.com
+            </p>
+            <p>
+              No: (+234 12 4344 455)
+            </p>
+            <p>
+              Address: No, 12 E-poll Street, Abuja, Nigeria
+            </p>
+          </div>
+        </div>
+        <div className="lp_footer_div_three">
+          <div>
+            <h2>Follow us</h2>
+          </div>
+          <div className="">
+            <p>
+              facebook.com/
+            </p>
+            <p>
+              instragrams.com
+            </p>
+            <p>
+              twitter.com
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
